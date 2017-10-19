4cf59af9126636b05dcf8be0bc0685dcdc7ecc69
id <id@wildapricot.com>
2017-10-19 16:30:55 +0300

    start second video pluralsight equality and comparisons_ex1

---
+++ Pluralsight.CSharpEquality.FoodEquals/Pluralsight.CSharpEquality.FoodEquals/Food.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+
+namespace Pluralsight.CSharpEquality.FoodEquals
+{
+    public class Food
+    {
+        private string _name;
+        public string Name { get { return _name; } }
+
+        public Food(string name)
+        {
+            this._name = name;
+        }
+
+        public override string ToString()
+        {
+            return _name;
+        }
+    }
+}
---
+++ Pluralsight.CSharpEquality.FoodEquals/Pluralsight.CSharpEquality.FoodEquals/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Pluralsight.CSharpEquality.FoodEquals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Food banana = new Food("banana");
+            Food banana2 = new Food("banana");
+            Food chocolate = new Food("chocolate");
+
+            Console.WriteLine(banana.Equals(chocolate));
+            Console.WriteLine(banana.Equals(banana2));
+        }
+    }
+}
